1a14d5f4a7b66efe64f65af1c7102e5a1feed130
Noice75 <matrriixx69@gmail.com>
2024-07-13 20:03:19 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDAn7c437CBDfT5tq9WKzs1kA93kyfLz8Y",
+    authDomain: "keshavindustriesweb.firebaseapp.com",
+    databaseURL: "https://keshavindustriesweb-default-rtdb.firebaseio.com",
+    projectId: "keshavindustriesweb",
+    storageBucket: "keshavindustriesweb.appspot.com",
+    messagingSenderId: "588137068461",
+    appId: "1:588137068461:web:2ff88bec0b0112eb4ed324",
+    measurementId: "G-ZC6R8PT121"
+  };
+export {firebaseConfig};
